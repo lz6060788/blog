58865f54d1219add21379a1bda4547d840e440ff
irises <912525302@qq.com>
2026-03-02 17:11:38 +0800

    fix: 更正 cherry-preview 导入路径
    - 将 @/components/cherry-preview 改为 @/components/editor/preview
    - 组件已重组到 editor/preview/ 目录

--- components/public/posts/article-content.tsx
+++ components/public/posts/article-content.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CherryPreview } from '@/components/cherry-preview'
+import { CherryPreview } from '@/components/editor/preview'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
